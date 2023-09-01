1a0e165c4387af2a69365510380ab8e12bfa0b8d
VladimirLakhtin <vllakhtin@gmail.com>
2023-09-01 11:57:41 +0300

    Fix trailing slash

--- megano/accounts/urls.py
+++ megano/accounts/urls.py
@@ -4,11 +4,11 @@ from accounts import views
 
 
 urlpatterns = [
-    path('sign-in/', views.SignInView.as_view(), name='login'),
-    path('sign-up/', views.SignUpView.as_view(), name='register'),
-    path('sign-out/', views.SignOutView.as_view(), name='logout'),
+    path('sign-in', views.SignInView.as_view(), name='login'),
+    path('sign-up', views.SignUpView.as_view(), name='register'),
+    path('sign-out', views.SignOutView.as_view(), name='logout'),
 
-    path('profile/', views.ProfileDetailsView.as_view(), name='profile'),
-    path('profile/password/', views.UpdatePasswordView.as_view(), name='change_password'),
-    path('profile/avatar/', views.UpdateAvatarView.as_view(), name='change_password'),
+    path('profile', views.ProfileDetailsView.as_view(), name='profile'),
+    path('profile/password', views.UpdatePasswordView.as_view(), name='change_password'),
+    path('profile/avatar', views.UpdateAvatarView.as_view(), name='change_password'),
 ]
--- megano/products/urls.py
+++ megano/products/urls.py
@@ -5,5 +5,5 @@ from products import views
 
 urlpatterns = [
     path('product/<int:pk>/', views.ProductDetailView.as_view(), name='products_details'),
-    path('product/<int:pk>/reviews/', views.CreateReviewView.as_view(), name='create_review'),
+    path('product/<int:pk>/reviews', views.CreateReviewView.as_view(), name='create_review'),
 ]
