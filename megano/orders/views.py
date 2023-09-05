1ba75da2fb9f0239cbde6d734a9a9c96b9c9eff1
VladimirLakhtin <vllakhtin@gmail.com>
2023-09-05 20:06:36 +0300

    add create order view

--- megano/orders/views.py
+++ megano/orders/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from cart.cart import Cart
 from orders.models import Order
-from orders.serializers import OrderSerializer
+from orders.serializers import OrderSerializer, CreateOrderSerializer
 
 
 class OrdersAPIView(APIView):
@@ -15,7 +16,19 @@ class OrdersAPIView(APIView):
         queryset = Order.objects.filter(profile=profile)
         serializer = OrderSerializer(queryset, many=True)
         return Response(serializer.data,
-            status=status.HTTP_200_OK)
+                        status=status.HTTP_200_OK)
 
     def post(self, request: Request) -> Response:
-        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        cart = Cart(request)
+        products_data = request.data
+        orders_data = {
+            'profile': request.user.profile.pk,
+            'products': [p.get('id') for p in products_data],
+        }
+        serializer = CreateOrderSerializer(data=orders_data, context={'cart': cart})
+
+        if serializer.is_valid():
+            order = serializer.save()
+            return Response(data={'orderId': order.pk},
+                            status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
