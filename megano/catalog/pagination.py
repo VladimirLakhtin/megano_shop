b42f469e86d09c7ae6d8d22b9262a319a79dff3b
VladimirLakhtin <vllakhtin@gmail.com>
2023-09-05 12:44:35 +0300

    write custom pagination

---
+++ megano/catalog/pagination.py
@@ -0,0 +1,16 @@
+from rest_framework import pagination
+from rest_framework.response import Response
+
+
+class CustomPagination(pagination.PageNumberPagination):
+    """Pagination for is designed for 20 elements per page"""
+
+    page_size = 20
+    page_query_param = 'currentPage'
+
+    def get_paginated_response(self, data) -> Response:
+        return Response({
+            'items': data,
+            'currentPage': self.page.number,
+            'lastPage': self.page.paginator.num_pages
+        })
