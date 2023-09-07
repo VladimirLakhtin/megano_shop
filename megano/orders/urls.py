a59e01bd6cc8419daf60c4fb23a024b9829c43b0
VladimirLakhtin <vllakhtin@gmail.com>
2023-09-07 15:33:42 +0300

    add order_details and payment urls

--- megano/orders/urls.py
+++ megano/orders/urls.py
@@ -4,5 +4,7 @@ from orders import views
 
 
 urlpatterns = [
-    path('orders', views.OrdersAPIView.as_view(), name='orders')
+    path('orders', views.OrdersAPIView.as_view(), name='orders'),
+    path('order/<int:id>', views.OrderDetailsAPIView.as_view(), name='order_details'),
+    path('payment/<int:id>', views.PaymentAPIView.as_view(), name='payment_user'),
 ]
