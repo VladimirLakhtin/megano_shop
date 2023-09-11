17646e4c5728eb38fbec5f6d187ff201a806abaf
VladimirLakhtin <vllakhtin@gmail.com>
2023-09-11 20:21:09 +0300

    add get_price method field

--- megano/cart/serializers.py
+++ megano/cart/serializers.py
@@ -7,7 +7,11 @@ class CartSerializer(CatalogSerializer):
     """Serializer for get data of products in cart"""
 
     count = serializers.SerializerMethodField()
+    price = serializers.SerializerMethodField()
 
     def get_count(self, obj):
         count = self.context.get(obj.pk)
         return count or self.context['cart'][str(obj.pk)]['count']
+
+    def get_price(self, obj):
+        return obj.salePrice or obj.price
